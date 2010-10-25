6ec55abc6a27cb15687cc4608fe2e5eeaee66170
arvicco <arvitallian@gmail.com>
2010-10-25 18:20:44 +0400

    object.subscribe[Event] added as sugar for object.Event.subscribe

--- spec/event_manager_spec.rb
+++ spec/event_manager_spec.rb
@@ -26,6 +26,7 @@ describe AMQP::Events::EventManager, " when initialized" do
 
     event = subject.subscribe('#.log.#'){|key, data| p key, data}
     event.should be_an AMQP::Events::Event
+    p object.events
 
 
   end
